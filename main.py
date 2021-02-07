3d36309cf12c7adc2675ff4ec358bf933359d59e
Luis Cabral <oluiscabral@hotmail.com>
2021-02-07 19:54:46 -0300

    update main.py

--- main.py
+++ main.py
@@ -11,15 +11,18 @@ stockopedia_website = StockopediaWebsite()
 def scrap_func():
     global stock_ticker_var
     stock_ticker = stock_ticker_var.get()
+    if len(stock_ticker.strip()) == 0:
+        return None
     stockopedia_website.login(Config.get_email(), Config.get_password())
     main_webpage = stockopedia_website.get_main_webpage(stock_ticker)
     if main_webpage == None:
-        print('Did not find a page for',stock_ticker)
+        print('Did not find a page for',stock_ticker,'stock ticker')
         return None
     scraped_data: MainScrapedData = main_webpage.scrap()
     formatted_datas = JamesTemplate.format(scraped_data)
     GoogleSpreadsheet.login()
     GoogleSpreadsheet.fill(formatted_datas)
+    print('Data extraction completed!')
 
 window = tk.Tk()
 window.title('Stockopedia Scraper')
